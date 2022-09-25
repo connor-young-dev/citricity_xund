31131b4eee6b3ca27ea8919c40285dd75b8babaf
Connor Young <connor.young.dev@gmail.com>
2022-09-25 16:11:08 +0100

    setup scss file structure

--- settings.php
+++ settings.php
@@ -11,40 +11,7 @@ if ($ADMIN->fulltree) {
     $settings = new theme_boost_admin_settingspage_tabs('themesettingcitricityxund', get_string('configtitle', 'theme_citricityxund'));             
                                                                                                                                     
     // Each page is a tab - the first is the "General" tab.                                                                         
-    $page = new admin_settingpage('theme_citricityxund_general', get_string('generalsettings', 'theme_citricityxund'));                             
-                                                                                                                                    
-    // Replicate the preset setting from boost.                                                                                     
-    $name = 'theme_citricityxund/preset';                                                                                                   
-    $title = get_string('preset', 'theme_citricityxund');                                                                                   
-    $description = get_string('preset_desc', 'theme_citricityxund');                                                                        
-    $default = 'default.scss';                                                                                                      
-                                                                                                                                    
-    // We list files in our own file area to add to the drop down. We will provide our own function to                              
-    // load all the presets from the correct paths.                                                                                 
-    $context = context_system::instance();                                                                                          
-    $fs = get_file_storage();                                                                                                       
-    $files = $fs->get_area_files($context->id, 'theme_citricityxund', 'preset', 0, 'itemid, filepath, filename', false);                    
-                                                                                                                                    
-    $choices = [];                                                                                                                  
-    foreach ($files as $file) {                                                                                                     
-        $choices[$file->get_filename()] = $file->get_filename();                                                                    
-    }                                                                                                                               
-    // These are the built in presets from Boost.                                                                                   
-    $choices['default.scss'] = 'default.scss';                                                                                      
-    $choices['plain.scss'] = 'plain.scss';                                                                                          
-                                                                                                                                    
-    $setting = new admin_setting_configselect($name, $title, $description, $default, $choices);                                     
-    $setting->set_updatedcallback('theme_reset_all_caches');                                                                        
-    $page->add($setting);                                                                                                           
-                                                                                                                                    
-    // Preset files setting.                                                                                                        
-    $name = 'theme_citricityxund/presetfiles';                                                                                              
-    $title = get_string('presetfiles','theme_citricityxund');                                                                               
-    $description = get_string('presetfiles_desc', 'theme_citricityxund');                                                                   
-                                                                                                                                    
-    $setting = new admin_setting_configstoredfile($name, $title, $description, 'preset', 0,                                         
-        array('maxfiles' => 20, 'accepted_types' => array('.scss')));                                                               
-    $page->add($setting);     
+    $page = new admin_settingpage('theme_citricityxund_general', get_string('generalsettings', 'theme_citricityxund'));                                                                                                                                 
 
     // Variable $brand-color.                                                                                                       
     // We use an empty default value because the default colour should come from the preset.                                        
