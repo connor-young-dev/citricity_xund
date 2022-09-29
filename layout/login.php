fc9d4c0541fc5b68660a12f832332f3771aee0f8
Connor Young <connor.young.dev@gmail.com>
2022-09-29 16:24:14 +0100

    Login page

--- layout/login.php
+++ layout/login.php
@@ -7,7 +7,8 @@ $bodyattributes = $OUTPUT->body_attributes();
 $templatecontext = [
     'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
     'output' => $OUTPUT,
-    'bodyattributes' => $bodyattributes
+    'bodyattributes' => $bodyattributes,
+    'logourl' => $OUTPUT->image_url('loginlogo', 'theme_citricityxund')
 ];
 
 echo $OUTPUT->render_from_template('theme_citricityxund/login', $templatecontext);
